a2c5d4756daf7e24a2ba4b58ed5b40d981af70f6
Allyy02 <98034572+Allyy02@users.noreply.github.com>
2024-03-31 00:19:11 -0400

    update controllers

--- App/Controllers/controllers.py
+++ App/Controllers/controllers.py
@@ -1,5 +1,8 @@
 from joblib import load
 import pandas as pd
+from flask import session
+from ProjectFiles.DataPreprocessing.data_cleaning import *
+import os
 
 def convert_to_years_months(total_years):
     # Separate the year into its integer and fractional components
@@ -14,17 +17,24 @@ def convert_to_years_months(total_years):
     return years_int, months  # Or return f"{years_int} years, {months} months" for a string representation
 
 
-def get_employee_tenure_predictions():
+def load_data(filename):
+    file_extension = os.path.splitext(filename)[1]
+    read_functions = {
+        '.xlsx': pd.read_excel,
+        '.xls': pd.read_excel,
+        '.csv': lambda f: pd.read_csv(f, converters={'satisfaction_level':percentConverterCSV})
+    }
+    read_function = read_functions.get(file_extension)
+    if read_function is None:
+        raise ValueError('Unsupported file type')
+    return read_function(filename)
 
-    loaded_model = load('App\\Controllers\\best_model.joblib')
-    
-    df = pd.read_excel('App\\uploads\\HR_Employee_Data.xlsx')
-    
+def get_employee_tenure_predictions():
+    loaded_model = load('App/Controllers/best_model.joblib')
+    filename = 'App/uploads/' + session.get('filename')
+    df = load_data(filename)
     clean_df = df[['satisfaction_level', 'number_project']]
-    
     new_predictions = loaded_model.predict(clean_df)
-    
     predictions_in_years_months = [convert_to_years_months(pred) for pred in new_predictions]
-    
-    
     return predictions_in_years_months
+
