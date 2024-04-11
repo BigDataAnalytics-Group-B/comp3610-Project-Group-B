008630f2615f0a28c0cd13d8ecc96c40193bc90b
Avin8888 <avinash.roopnarine08@gmail.com>
2024-04-10 23:10:42 -0400

    Anomaly Findings to Frontend

--- App/Controllers/controllers.py
+++ App/Controllers/controllers.py
@@ -48,5 +48,52 @@ def get_employee_tenure_predictions():
     
     return results
 
+def get_employee_anomalies():
+    
+    loaded_model = load('ProjectFiles/Models/anomaly_model.joblib')
+    filename = 'App/uploads/' + session.get('filename')
+    
+    df = load_data(filename)
+
+    if 'average_montly_hours' in df.columns:
+        df['average_monthly_hours'] = df['average_montly_hours'].copy()
+        df.drop(columns=['average_montly_hours'], inplace=True)
+    
+    df = pd.get_dummies(df, columns=['Department', 'salary'])
+
+    df.dropna(inplace=True)
+    emp_ids = df['Emp_Id']
+
+    df = df.drop(['Emp_Id'], axis=1)
+    
+    work_hours = df['average_monthly_hours'].values.reshape(-1, 1)
+
+    lm_outliers = loaded_model.predict(work_hours)
+
+    lm_anomaly_indices = df.index[lm_outliers == -1]
+
+    lm_anomaly_df = df.iloc[lm_anomaly_indices]
+
+    lm_emp_ids = emp_ids.iloc[lm_anomaly_df.index.values]
+
+    lm_average_monthly_hours = lm_anomaly_df['average_monthly_hours']
+
+    results_df = pd.DataFrame({
+        'Emp_Id': lm_emp_ids,
+        'average_monthly_hours': lm_average_monthly_hours
+    })
+    
+    results_list = results_df.to_dict(orient='records')
+    
+    # print(results_list)
+    
+    return results_list
+
+    
+    
+
+    
+    
+
 
 
